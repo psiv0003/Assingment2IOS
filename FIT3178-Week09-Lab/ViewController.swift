723b7b973f04613cd209c735ea04fb34adf5d88a
Joshua <raynavia1992@gmail.com>
2019-03-26 11:27:25 +1100

    Initial Commit

---
+++ FIT3178-Week09-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT3178-Week09-Lab
+//
+//  Created by Joshua Olsen on 26/3/19.
+//  Copyright © 2019 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
